3350bc01950a2c2da429e96f49126f104c5a6426
Amit Chaurasia <amit.anuaum@gmail.com>
2020-08-30 11:24:35 +0530

    chore(all): Initial Commit - Setup angular based image browsing app

---
+++ README.md
@@ -0,0 +1,15 @@
+# Image Browser App
+
+## Features
+
+- The app fetches a list of photos and display them in a space-optimized grid.
+- User can search the photo using a keyword.
+- On click of the image user can zoom in and zoom out the image.
+- User can also like or remove the like from the image.
+
+## Setup Development Environment
+
+1. Run `git clone git@github.com:javascript-architect/image-browser.git` to clone the repository.
+2. Run `cd image-browser` to change the directory to `image-browser`.
+3. Run `npm install` to install the dependencies.
+4. Run `npm start` for a dev server. Navigate to `http://localhost:4200/`.
---
+++ scripts/setenv.ts
@@ -0,0 +1,31 @@
+import { writeFile } from 'fs';
+import { argv } from 'yargs';
+import { config } from 'dotenv';
+
+// read environment variables from .env file
+config();
+
+// read the command line arguments passed with yargs
+const environment = argv.environment;
+const isProduction = environment === 'production';
+
+const targetPath = isProduction
+  ? `./src/environments/environment.prod.ts`
+  : `./src/environments/environment.ts`;
+
+const content = `
+export const environment = {
+  production: ${isProduction},
+  UNSPLASH_API: "${process.env.UNSPLASH_API}",
+  UNSPLASH_ACCESS_KEY: "${process.env.UNSPLASH_ACCESS_KEY}",
+  UNSPLASH_SECRET_KEY: "${process.env.UNSPLASH_SECRET_KEY}",
+};
+`;
+
+writeFile(targetPath, content, (err) => {
+  if (err) {
+    console.error(err);
+  }
+
+  console.log(`Wrote variables to ${targetPath}`);
+});
